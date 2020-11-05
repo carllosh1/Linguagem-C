336c833556cc8620a73e3fd7ffe39343ebb5c998
Carlos Henrique Oliveira Santos <69221726+carllosh1@users.noreply.github.com>
2020-11-05 09:59:57 -0300

    Update Exercicio10.c

--- Modularização(Teste)/Exercicio10.c
+++ Modularização(Teste)/Exercicio10.c
@@ -1,24 +1,27 @@
 /* Escreva um procedimento chamado AUMENTO que receba dois valores
-reais X e Y como par‚metros e aumente o valor de X em Y%. */
+reais X e Y como par√¢metros e aumente o valor de X em Y%. */
 
 #include<stdio.h>
 
-void aumento(){
-	float x,y;
+
+void aumento(float x, float y){
+	float resultado;
+		
+	resultado = x + ((y/100)*x);
+	
+	printf("o valor de X aumentou em + %Y: %0.2f", resultado);
+}
+
+
+
+int main(){
+	float x,y, resultado;
 	
 	printf("Entre com X: ");
 	scanf("%f", &x);
 	
-	printf("Em quanto voce deseja aumentar X?: 0.1x");
+	printf("Em quanto voce deseja aumentar X?: ");
 	scanf("%f", &y);
-	
- 	 x+= y* 0.1;
-	
-	printf("o valor de X aumentou em + %Y: %0.2f", x);
-	
-}
-
-int main(){
-	
-	aumento();
+		
+	aumento(x,y);
 }
